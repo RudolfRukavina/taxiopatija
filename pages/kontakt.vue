cd940d1bbf72b0bf9c915e565b123bc37f132f52
RudolfRukavina <rukavina.rudi@gmail.com>
2024-01-29 16:21:31 +0100

    v0.23

--- pages/kontakt.vue
+++ pages/kontakt.vue
@@ -3,8 +3,12 @@
     class="pt-24  dark:ring-white/10 ring-primary/5 bg-gradient-to-b from-indigo-100 from-10% via-sky-100 via-30% to-blue-200 to-90% shadow-xl items-center flex flex-col p-8 w-full bg-white">
     <NuxtLink to="/" class='absolute left-0  top-14 mx-5 text-3xl rounded-xl px-3 p-5'><i class=" fa fa-chevron-left"></i>
     </NuxtLink>
-    <a href="tel:+385917865619" class='absolute right-0  top-14 mx-5 text-3xl rounded-xl px-3 p-5'><i
-        class=" fa fa-phone"></i>
+    <a href="tel:+385917865619" class='absolute right-0  top-14 mx-5 text-3xl rounded-xl px-3 p-5'>
+      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
+        class="w-6 h-6">
+        <path stroke-linecap="round" stroke-linejoin="round"
+          d="M2.25 6.75c0 8.284 6.716 15 15 15h2.25a2.25 2.25 0 002.25-2.25v-1.372c0-.516-.351-.966-.852-1.091l-4.423-1.106c-.44-.11-.902.055-1.173.417l-.97 1.293c-.282.376-.769.542-1.21.38a12.035 12.035 0 01-7.143-7.143c-.162-.441.004-.928.38-1.21l1.293-.97c.363-.271.527-.734.417-1.173L6.963 3.102a1.125 1.125 0 00-1.091-.852H4.5A2.25 2.25 0 002.25 4.5v2.25z" />
+      </svg>
     </a>
     <div class="relative flex items-center gap-x-4 ">
       <img
@@ -36,17 +40,17 @@
 
 
           <div class="flex flex-col items-center justify-center text-center">
-            <span class="p-3  text-blue-500 rounded-full bg-blue-100/80 dark:bg-gray-800">
+            <a class="p-3  text-blue-500 rounded-full bg-blue-100/80 dark:bg-gray-800" href="tel:+385917865619">
               <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
                 stroke="currentColor" class="w-6 h-6">
                 <path stroke-linecap="round" stroke-linejoin="round"
                   d="M2.25 6.75c0 8.284 6.716 15 15 15h2.25a2.25 2.25 0 002.25-2.25v-1.372c0-.516-.351-.966-.852-1.091l-4.423-1.106c-.44-.11-.902.055-1.173.417l-.97 1.293c-.282.376-.769.542-1.21.38a12.035 12.035 0 01-7.143-7.143c-.162-.441.004-.928.38-1.21l1.293-.97c.363-.271.527-.734.417-1.173L6.963 3.102a1.125 1.125 0 00-1.091-.852H4.5A2.25 2.25 0 002.25 4.5v2.25z" />
               </svg>
-            </span>
+            </a>
 
             <h2 class="mt-4 text-lg font-medium text-gray-800 dark:text-white">Telefon</h2>
             <p class="mt-2 text-gray-500 dark:text-gray-400">Nazovite nas!</p>
-            <p class="mt-2 text-blue-500 dark:text-blue-400">+1 (555) 000-0000</p>
+            <a href="tel:+385917865619" class="mt-2 text-blue-500 dark:text-blue-400">+1 (555) 000-0000</a>
           </div>
         </div>
       </div>
