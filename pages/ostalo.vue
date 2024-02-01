020950ecc860bd7a2836a0adb5766f062ca44450
RudolfRukavina <rukavina.rudi@gmail.com>
2024-02-01 12:00:38 +0100

    v0.88

--- pages/ostalo.vue
+++ pages/ostalo.vue
@@ -1,161 +1,67 @@
 <template>
-  <section class="bg-gray-100  py-8 " id='ostalo'>
-    <div class="container mx-auto flex flex-wrap pt-4 pb-12">
-      <h2 class="w-full my-2 text-5xl font-bold leading-tight text-center text-gray-800">
-        Ostale usluge
-      </h2>
-      <div class="w-full mb-4">
-        <div class="h-1 mx-auto gradientline w-64 opacity-25 my-0 py-0 rounded-t"></div>
-      </div>
-      <div class="w-full md:w-1/3 p-6 flex flex-col flex-grow flex-shrink">
-        <div class="flex-1 bg-white rounded-t rounded-b-none overflow-hidden shadow">
-          <NuxtLink to="#seminari" class="flex flex-wrap no-underline hover:no-underline">
-            <div class="w-full font-bold text-2xl pt-3 text-gray-800 px-6">
-              Pomoć u pisanju
-            </div>
-            <div>
-              <li class="text-gray-800 text-lg px-6 ">
-                Zadaća
-              </li>
-              <li class="text-gray-800 text-lg px-6 ">
-                Seminara
-              </li>
-              <li class="text-gray-800 text-lg px-6">
-                Projekata
-              </li>
-              <li class="text-gray-800 text-lg px-6">
-                Lektira
-              </li>
-            </div>
+  <body class="leading-normal tracking-normal text-white gradient" style="font-family: 'Source Sans Pro', sans-serif;">
+    <div class="py-10 mx-1" id='naslovnica'>
+      <div class="container px-3 mx-auto flex flex-wrap flex-col md:flex-row items-center">
+        <!--Left Col-->
+        <div class="flex flex-col w-full md:w-3/5 justify-center items-start text-center md:text-left">
+          <p class=" tracking-loose w-full">Bez puno muke, uz Parallel poduke!</p>
+          <h1 class="mb-4  text-5xl lg:text-6xl font-bold leading-tight w-full">
+            Ostale usluge
+          </h1>
+          <p class="leading-normal text-lg mb-8">
+            Tu smo da vam pomognemo!
+          </p>
+          <NuxtLink to="#cjenik"
+            class="mx-auto lg:mx-0 hover:underline bg-white text-gray-800 font-bold rounded-full my-6 py-4 px-8 shadow-lg focus:outline-none focus:shadow-outline transform transition hover:scale-105 duration-300 ease-in-out">
+            Nazovite nas!
           </NuxtLink>
         </div>
-        <div class="flex-none mt-auto bg-white rounded-b rounded-t-none overflow-hidden shadow p-6">
-          <div class="flex items-center justify-start">
-            <div class="w-full font-bold text-2xl pt-3 text-gray-800 ">
-              Priprema za natjecanja
-            </div>
-          </div>
+        <!--Right Col-->
+        <div class="w-full md:w-2/5 py-6 text-center">
+          <img class="w-full md:w-4/5 z-50 rounded-xl" src="../assets/img/ostaleusluge.png" />
 
-        </div>
-        <div class="flex-none mt-auto bg-white rounded-b rounded-t-none overflow-hidden shadow p-6">
-          <div class="flex items-center justify-start">
-            <div class="w-full font-bold text-2xl pt-3 text-gray-800 ">
-              Organizacija seminara
-            </div>
-          </div>
 
-        </div>
-        <div class="flex-none mt-auto bg-white rounded-b rounded-t-none overflow-hidden shadow p-6">
-          <div class="flex items-center justify-start">
-            <div class="w-full font-bold text-2xl pt-3 text-gray-800 ">
-              Edukacija iz različitih područja
-            </div>
-          </div>
-
-        </div>
-      </div>
-      <div class="w-full md:w-1/3 p-6 flex flex-col flex-grow flex-shrink">
-        <div class="flex-1 bg-white rounded-t rounded-b-none overflow-hidden shadow">
-          <NuxtLink to="#pisanje" class="flex flex-wrap no-underline hover:no-underline">
-            <div class="w-full pt-3 font-bold text-2xl text-gray-800 px-6">
-              Pomoć u pisanju
-            </div>
-            <p class="text-gray-800 text-base px-6 mb-5">
-              Cjenik provođenja
-            </p>
-          </NuxtLink>
-        </div>
-        <div class="flex-none mt-auto bg-white rounded-b rounded-t-none overflow-hidden shadow p-6">
-          <div class="flex items-center justify-center">
-            <table class="w-full text-sm text-left rtl:text-right border-2 text-gray-500 dark:text-gray-400">
-              <thead class="text-xs text-white uppercase bg-slate-900 ">
-                <tr>
-                  <th scope="col" class=" py-3 text-base text-start px-6">
-
-                  </th>
-                  <th scope="col" class="py-3 text-start px-6">
-                    školski sat
-                  </th>
-                </tr>
-              </thead>
-              <tbody>
-                <tr
-                  class="odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 border-b dark:border-gray-700">
-                  <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                    Individualno
-                  </th>
-                  <td class="px-6 py-4 font-bold">
-                    10,00 €
-                  </td>
-                </tr>
-                <tr
-                  class="odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 border-b dark:border-gray-700">
-                  <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                    Grupno
-                  </th>
-                  <td class="px-6 py-4 font-bold">
-                    7,50 €
-                  </td>
-                </tr>
-              </tbody>
-            </table>
-          </div>
-        </div>
-      </div>
-      <div class="w-full md:w-1/3 p-6 flex flex-col flex-grow flex-shrink">
-        <div class="flex-1 bg-white rounded-t rounded-b-none overflow-hidden shadow">
-          <a to="'#natjecanja" class="flex flex-wrap no-underline hover:no-underline">
-            <div class="w-full pt-3 font-bold text-2xl text-gray-800 px-6">
-              Priprema za natjecanja
-            </div>
-            <p class="text-gray-800 text-base px-6 mb-5">
-              Cjenik provođenja
-            </p>
-          </a>
-        </div>
-        <div class="flex-none mt-auto bg-white rounded-b rounded-t-none overflow-hidden shadow p-6">
-          <div class="flex items-center justify-end">
-            <table class="w-full text-sm text-left rtl:text-right border-2 text-gray-500 dark:text-gray-400">
-              <thead class="text-xs text-white uppercase bg-slate-900 ">
-                <tr>
-                  <th scope="col" class=" py-3 text-base text-start px-6">
-
-                  </th>
-                  <th scope="col" class="py-3 text-start px-6">
-                    školski sat
-                  </th>
-                </tr>
-              </thead>
-              <tbody>
-                <tr
-                  class="odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 border-b dark:border-gray-700">
-                  <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                    Individualno
-                  </th>
-                  <td class="px-6 py-4 font-bold">
-                    10,00 €
-                  </td>
-                </tr>
-                <tr
-                  class="odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 border-b dark:border-gray-700">
-                  <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                    Grupno
-                  </th>
-                  <td class="px-6 py-4 font-bold">
-                    7,50 €
-                  </td>
-                </tr>
-              </tbody>
-            </table>
-          </div>
         </div>
       </div>
     </div>
-  </section>
+    <div class="relative -mt-12 lg:-mt-24">
+      <svg viewBox="0 0 1428 174" version="1.1" xmlns="http://www.w3.org/2000/svg"
+        xmlns:xlink="http://www.w3.org/1999/xlink">
+        <g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+          <g transform="translate(-2.000000, 44.000000)" fill="#FFFFFF" fill-rule="nonzero">
+            <path
+              d="M0,0 C90.7283404,0.927527913 147.912752,27.187927 291.910178,59.9119003 C387.908462,81.7278826 543.605069,89.334785 759,82.7326078 C469.336065,156.254352 216.336065,153.6679 0,74.9732496"
+              opacity="0.100000001"></path>
+            <path
+              d="M100,104.708498 C277.413333,72.2345949 426.147877,52.5246657 546.203633,45.5787101 C666.259389,38.6327546 810.524845,41.7979068 979,55.0741668 C931.069965,56.122511 810.303266,74.8455141 616.699903,111.243176 C423.096539,147.640838 250.863238,145.462612 100,104.708498 Z"
+              opacity="0.100000001"></path>
+            <path
+              d="M1046,51.6521276 C1130.83045,29.328812 1279.08318,17.607883 1439,40.1656806 L1439,120 C1271.17211,77.9435312 1140.17211,55.1609071 1046,51.6521276 Z"
+              id="Path-4" opacity="0.200000003"></path>
+          </g>
+          <g transform="translate(-4.000000, 76.000000)" fill="#FFFFFF" fill-rule="nonzero">
+            <path
+              d="M0.457,34.035 C57.086,53.198 98.208,65.809 123.822,71.865 C181.454,85.495 234.295,90.29 272.033,93.459 C311.355,96.759 396.635,95.801 461.025,91.663 C486.76,90.01 518.727,86.372 556.926,80.752 C595.747,74.596 622.372,70.008 636.799,66.991 C663.913,61.324 712.501,49.503 727.605,46.128 C780.47,34.317 818.839,22.532 856.324,15.904 C922.689,4.169 955.676,2.522 1011.185,0.432 C1060.705,1.477 1097.39,3.129 1121.236,5.387 C1161.703,9.219 1208.621,17.821 1235.4,22.304 C1285.855,30.748 1354.351,47.432 1440.886,72.354 L1441.191,104.352 L1.121,104.031 L0.457,34.035 Z">
+            </path>
+          </g>
+        </g>
+      </svg>
+    </div>
+
+  </body>
 </template>
 
 <style scoped>
 .gradientline {
   background: linear-gradient(90deg, #d53369fb 0%, #daaf51 100%);
+}
+
+.gradient {
+  background: #00B4DB;
+  /* fallback for old browsers */
+  background: -webkit-linear-gradient(to right, #008e96, #00ccaa);
+  /* Chrome 10-25, Safari 5.1-6 */
+  background: linear-gradient(to right, #008e96, #00ccaa);
+  /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
 }
 </style>
