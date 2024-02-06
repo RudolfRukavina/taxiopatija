0537419d9fa2bfdbbe87830271358dad5fa7d82b
RudolfRukavina <rukavina.rudi@gmail.com>
2024-02-06 23:06:16 +0100

    v1.103

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -10,7 +10,7 @@ export default defineNuxtConfig({
     robots: {
     mergeWithRobotsTxtPath: './public/_robots.txt'
   },
-    ssr: false,
+
     modules: [
     '@nuxtjs/tailwindcss',
       'nuxt-swiper',
