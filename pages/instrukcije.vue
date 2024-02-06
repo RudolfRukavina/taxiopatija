6059de4514e0169f838e8217689d511ad769148f
RudolfRukavina <rukavina.rudi@gmail.com>
2024-02-07 00:47:01 +0100

    v1.24

--- pages/instrukcije.vue
+++ pages/instrukcije.vue
@@ -34,74 +34,11 @@
 
 
 
-            <div id="default-carousel" class="relative w-full rounded-xl " data-carousel="static">
-              <!-- Carousel wrapper -->
-              <div class="relative h-96 overflow-hidden rounded-lg md:h-[580px]">
-                <!-- Item 1 -->
-                <div class="hidden duration-700 ease-in-out rounded-xl" data-carousel-item>
-                  <img src="../assets/img/osnovnaskola.jpeg"
-                    class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2 rounded-xl" alt="...">
-                </div>
-                <!-- Item 2 -->
-                <div class="hidden duration-700 ease-in-out rounded-xl" data-carousel-item>
-                  <img src="../assets/img/srednjaskola.jpeg"
-                    class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2 rounded-xl" alt="...">
-                </div>
-                <!-- Item 3 -->
-                <div class="hidden duration-700 ease-in-out rounded-xl" data-carousel-item>
-                  <img src="../assets/img/fakultet.jpeg"
-                    class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2 rounded-xl" alt="...">
-                </div>
-                <div class="hidden duration-700 ease-in-out rounded-xl" data-carousel-item>
-                  <img src="../assets/img/ucionica.jpeg"
-                    class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2 rounded-xl" alt="...">
-                </div>
-                <div class="hidden duration-700 ease-in-out rounded-xl" data-carousel-item>
-                  <img src="../assets/img/malaucionica.jpeg"
-                    class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2 rounded-xl" alt="...">
-                </div>
+            <div class="w-full md:w-4/5 py-6 text-center">
 
-              </div>
-              <!-- Slider indicators -->
-              <div class="absolute z-30 flex -translate-x-1/2 bottom-5 left-1/2 space-x-3 rtl:space-x-reverse">
-                <button type="button" class="w-3 h-3 rounded-full" aria-current="true" aria-label="Slide 1"
-                  data-carousel-slide-to="0"></button>
-                <button type="button" class="w-3 h-3 rounded-full" aria-current="false" aria-label="Slide 2"
-                  data-carousel-slide-to="1"></button>
-                <button type="button" class="w-3 h-3 rounded-full" aria-current="false" aria-label="Slide 3"
-                  data-carousel-slide-to="2"></button>
-                <button type="button" class="w-3 h-3 rounded-full" aria-current="false" aria-label="Slide 4"
-                  data-carousel-slide-to="3"></button>
-                <button type="button" class="w-3 h-3 rounded-full" aria-current="false" aria-label="Slide 5"
-                  data-carousel-slide-to="4"></button>
-              </div>
-              <!-- Slider controls -->
-              <button type="button"
-                class="absolute top-0 start-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none"
-                data-carousel-prev>
-                <span
-                  class="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
-                  <svg class="w-4 h-4 text-white dark:text-gray-800 rtl:rotate-180" aria-hidden="true"
-                    xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
-                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                      d="M5 1 1 5l4 4" />
-                  </svg>
-                  <span class="sr-only">Previous</span>
-                </span>
-              </button>
-              <button type="button"
-                class="absolute top-0 end-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none"
-                data-carousel-next>
-                <span
-                  class="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
-                  <svg class="w-4 h-4 text-white dark:text-gray-800 rtl:rotate-180" aria-hidden="true"
-                    xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
-                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                      d="m1 9 4-4-4-4" />
-                  </svg>
-                  <span class="sr-only">Next</span>
-                </span>
-              </button>
+              <img class="w-full md:w-4/5 z-50 rounded-xl" src="../assets/img/osnovnaskola.jpeg" v-show='count === 1' />
+              <img class="w-full md:w-4/5 z-50 rounded-xl" src="../assets/img/srednjaskola.jpeg" v-show='count === 2' />
+              <img class="w-full md:w-4/5 z-50 rounded-xl" src="../assets/img/fakultet.jpeg" v-show='count === 3' />
             </div>
 
           </div>
@@ -370,37 +307,14 @@
 <script setup>
   import { ref } from "vue"
   import { onMounted } from 'vue'
-  import {
-    initAccordions,
-    initCarousels,
-    initCollapses,
-    initDials,
-    initDismisses,
-    initDrawers,
-    initDropdowns,
-    initModals,
-    initPopovers,
-    initTabs,
-    initTooltips
-  } from 'flowbite'
 
-  const initializeComponents = () => {
-    initAccordions();
-    initCarousels();
-    initCollapses();
-    initDials();
-    initDismisses();
-    initDrawers();
-    initDropdowns();
-    initModals();
-    initPopovers();
-    initTabs();
-    initTooltips();
-  };
+
 
   // Initialize components on client side
-  onNuxtReady(() => {
-    initializeComponents();
+
+  const count = ref(1)
+  onMounted(() => {
+    setInterval(() => { count.value = (count.value % 3) + 1; }, 10000);
   });
 
 
@@ -420,8 +334,8 @@
     ogTitle: 'Instrukcije | Poduke Parallel',
     description: 'Bez puno muke, uz Parallel poduke! Održavamo instrukcije iz svih obaveznih i velikog broja stručnih predmeta!',
     ogDescription: 'Bez puno muke, uz Parallel poduke! Održavamo instrukcije iz svih obaveznih i velikog broja stručnih predmeta!',
-    ogImage: '../public/instrukcijecjenik.png',
-    twitterCard: '../public/instrukcijecjenik.png',
+    ogImage: '@/public/instrukcijecjenik.png',
+    twitterCard: '@/public/instrukcijecjenik.png',
   })
 
 
@@ -437,5 +351,15 @@
   background: linear-gradient(to right, #1CB5E0, #000046);
   /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
 
+}
+
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.5s;
+}
+
+.fade-enter,
+.fade-leave-to {
+  opacity: 0;
 }
 </style>
