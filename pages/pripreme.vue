6d573e26b8fedd91944eeb85f96e4c169d9f7900
RudolfRukavina <rukavina.rudi@gmail.com>
2024-02-06 22:48:58 +0100

    v1.09

--- pages/pripreme.vue
+++ pages/pripreme.vue
@@ -249,7 +249,7 @@
               <label for="message" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-400">Vaša
                 poruka</label>
               <textarea id="message" rows="10" v-model="selectedItemsText"
-                class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg shadow-sm border border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light"
+                class="block p-2.5 w-full text-[16px] text-gray-900 bg-gray-50 rounded-lg shadow-sm border border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light"
                 placeholder=""></textarea>
 
             </div>
